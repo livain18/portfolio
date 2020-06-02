fdeb9b603d1e915ef029117954e9274196cf2fb5
Lieven <lieven.vdiest@gmail.com>
2020-06-02 11:25:57 +0200

    fixing php email and mobile nav issues

--- sendcontact.php
+++ sendcontact.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "John Doe" ); //UPDATE THIS TO YOUR NAME
-define( "RECIPIENT_EMAIL", "john@example.com" ); //UPDATE THIS TO YOUR EMAIL ID
+define( "RECIPIENT_NAME", "Lieven Vandiest" ); //UPDATE THIS TO YOUR NAME
+define( "RECIPIENT_EMAIL", "lieven@greatcompany.be" ); //UPDATE THIS TO YOUR EMAIL ID
 define( "EMAIL_SUBJECT", "Website Visitor Message" ); //UPDATE THIS TO YOUR SUBJECT
 
 // Read the form values
